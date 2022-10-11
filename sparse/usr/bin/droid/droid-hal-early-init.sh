54d61be25eaa0716b87c99e7765b5acca4ba4158
aymanrgab <aymanrar2c@gmail.com>
2022-10-11 08:55:49 +0200

    Fix vpn

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -6,8 +6,11 @@ fi
 mount -o bind /system/lib64/hw/audio.hidl_compat.default.so /vendor/lib64/hw/audio.primary.default.so
 mount -o bind /dev/null /vendor/etc/init/hw/init.samsungexynos7880.usb.rc
 
+mknod /dev/tun c 10 200
+chmod 660 /dev/tun
+chown system:vpn /dev/tun
+
 if [[ ! -e /mnt/vendor ]]; then
     mkdir -p /mnt/vendor
     ln -s /efs /mnt/vendor/
 fi
-
